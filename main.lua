173f405d2f39d2aae36705ea989e737ba994bbbe
arc <picklesthedog1212@gmail.com>
2025-11-16 19:24:19 +1100

    main mod oh yeah im creaming

---
+++ main.lua
@@ -0,0 +1,67 @@
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
+
+
+SMODS.Atlas({
+    key = "CustomJokers", 
+    path = "CustomJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+local NFS = require("nativefs")
+to_big = to_big or function(a) return a end
+lenient_bignum = lenient_bignum or function(a) return a end
+
+local jokerIndexList = {1,2}
+
+local function load_jokers_folder()
+    local mod_path = SMODS.current_mod.path
+    local jokers_path = mod_path .. "/jokers"
+    local files = NFS.getDirectoryItemsInfo(jokers_path)
+    for i = 1, #jokerIndexList do
+        local file_name = files[jokerIndexList[i]].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+load_jokers_folder()
+SMODS.ObjectType({
+    key = "arcs_food",
+    cards = {
+        ["j_gros_michel"] = true,
+        ["j_egg"] = true,
+        ["j_ice_cream"] = true,
+        ["j_cavendish"] = true,
+        ["j_turtle_bean"] = true,
+        ["j_diet_cola"] = true,
+        ["j_popcorn"] = true,
+        ["j_ramen"] = true,
+        ["j_selzer"] = true
+    },
+})
+
+SMODS.ObjectType({
+    key = "arcs_arcs_jokers",
+    cards = {
+        ["j_arcs_overgrowth"] = true,
+        ["j_arcs_triggernometry"] = true
+    },
+})
