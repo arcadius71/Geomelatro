5fed84dfa1e3c4e257d20ff60b93a10ecd68d580
arc <picklesthedog1212@gmail.com>
2025-11-24 18:18:52 +1100

    please

--- Objects/jokers/5_celestial.lua
+++ Objects/jokers/5_celestial.lua
@@ -5,9 +5,6 @@ SMODS.Joker({
         "e_arcs_warm",
         "e_arcs_hot",
         "e_arcs_toasted",
-        "e_arcs_burnt",
-        "e_arcs_charcoal",
-        "e_arcs_ash",
     } } },
 
     cost = 11,
