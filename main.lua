13d7333b875bc1c0e61096d6ce718f489314ad9a
BitterrDoes <bitterdoes420@gmail.com>
2025-11-29 14:00:40 +1100

    WeWa i hate talisman but i looove it

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ lenient_bignum = lenient_bignum or function(a) return a end
 local geomelatro = SMODS.current_mod
 
 -- Load Files and Folders
+
 function geomelatro.Load_file(file) -- basically just SMODS.load_file() but safer, so i can accidentally have somethign break and it be chill
 	local chunk = SMODS.load_file(file, "geomelatro")
 	if chunk then
@@ -43,7 +44,12 @@ geomelatro.Load_Dir("Objects/jokers")
 geomelatro.Load_Dir("Objects/backs")
 geomelatro.Load_Dir("Objects/rarities")
 
+
+
 -- Grading stuff
+
+
+
 	-- All requirements
 local GradeReq = {
 	{ -- requires custom code, so make it free n shit
@@ -135,17 +141,25 @@ local GradeReq = {
 
 	-- Function to check requirements
 local function Grade() -- theres like 7 different "Grade" variables
-	local discards = G.GAME.current_round.discards_used
-	local hands = G.GAME.hands_played
-	local required = G.GAME.blind.chips
-	local score = G.GAME.chips
-	local over = score / required
+	local discards = to_big(G.GAME.current_round.discards_used)
+	local hands = to_big(G.GAME.current_round.hands_played)
+	local required = to_big(G.GAME.blind.chips)
+	local score = to_big(G.GAME.chips)
+	local over = to_big(score / required)
+	print("Discards: ".. discards, "Hands: ".. hands,
+		  "Score: ".. score, "Over: ".. over)
 
 	local hPoint = -4
 	local gradetbl
 
 	-- Check Score requirements
 	for _, grade in ipairs(GradeReq) do -- has to be ipairs since if its outta order A being before A+ would make A more priority than A+
+		print([[
+
+
+		]]..grade.grade..[[
+
+		]])
 		-- holy if statements | Def gonna want to beg balatro server to help me check if its good or not 😭
 		if grade.grade == "A++" then
 			-- custom stuff here
@@ -155,21 +169,40 @@ local function Grade() -- theres like 7 different "Grade" variables
 			goto continue
 		end
 		-- score
-		if grade.score[2] and grade.score[2] < over then
+		print(to_big(grade.score[2]) < over, "a2")
+		print(to_big(grade.score[1]) < over, "a1")
+		if to_big(grade.score[2]) < over then
 			grade.othermet = false
-		elseif grade.score[1] < over then
+		elseif to_big(grade.score[1]) < over then
 			grade.met = false
 		end
 
-		if grade.hands > hands then grade.met = false end
+		print(to_big(grade.hands) > hands, "b")
+		if to_big(grade.hands) > hands then grade.met = false end
 			-- fuck that we're doing it in one line
 			
-		if grade.discards > discards then grade.discards = false end
+		print(to_big(grade.discards) > discards, "c")
+		if to_big(grade.discards) > discards then grade.met = false end
 
-		if grade.othermet and grade.points > hPoint then
-			hPoint = grade.points
-			gradetbl = grade
+		print((grade.othermet or grade.met) and grade.points > hPoint, "d")
+		if (grade.othermet or grade.met) then
+			if grade.points > hPoint then
+				hPoint = grade.points
+				gradetbl = grade	
+			end
+			-- I found having them together broke it???? i still had the ()
 		end
+		grade.othermet = true
+		grade.met = true
+		print([[
+
+
+
+		:3
+
+
+
+		]])
 	    ::continue::
 	end
 
@@ -183,9 +216,9 @@ geomelatro.calculate = function(self, context)
 		else
 			G.GAME.Grades = 0
 		end
-	elseif context.blind_defeated then
+	elseif context.end_of_round and not context.game_over then
 		local grading = Grade()
-		print(grading)
+		print(grading.grade)
 	elseif context.end_of_round and context.game_over and context.main_eval and SMODS.find_card("j_mr_bones") then
 		G.GAME.Grades = G.GAME.Grades - 3
 	end
